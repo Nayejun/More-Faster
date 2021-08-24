b9a720ae28e80f10a2fb163532bdcdb9fad89747
4YJ <67637706+yejun0705@users.noreply.github.com>
2021-08-24 18:04:23 +0900

    Code-splitting With react-loadable

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import Loadable from "react-loadable";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Loading = () => {
+    return <div>Loading..</div>;
+}
+export const Home = Loadable({
+    Loader: () => import('./Home'),
+    Loading: Loading
+});
+export const About = Loadable({
+    Loader: () => import('./About'),
+    Loading: Loading
+});
